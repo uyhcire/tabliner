01255a9e40e23acadb9fb56ae5acf7c0341d70d0
uyhcire <ericyu3+uyhcire@gmail.com>
2019-05-12 17:44:54 -0700

    Use Tabs API

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 function App(): JSX.Element {
-  return <div>hello, world.</div>;
+  const [chromeTabs, setChromeTabs] = useState(null as Array<
+    chrome.tabs.Tab
+  > | null);
+  useEffect(() => {
+    chrome.tabs.query({}, tabs => setChromeTabs(tabs));
+  }, []);
+
+  return (
+    <div>
+      {(() => {
+        const keys = new Set();
+        for (const tab of chromeTabs || []) {
+          for (const key of Object.keys(tab)) {
+            keys.add(key);
+          }
+        }
+        return JSON.stringify([...keys]);
+      })()}
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("container"));
