c9e8acc584f1e46483dd82676ed2ff460c8f7aa8
uyhcire <ericyu3+uyhcire@gmail.com>
2019-05-12 18:15:09 -0700

    Show favicon and tab title in a spreadsheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,59 @@
+import Handsontable from "handsontable";
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { HotTable } from "@handsontable/react";
+import "handsontable/dist/handsontable.full.css";
 
-function App(): JSX.Element {
-  const [chromeTabs, setChromeTabs] = useState(null as Array<
-    chrome.tabs.Tab
-  > | null);
+// We have the `tabs` permission, so some fields are guaranteed to be present
+type ChromeTab = chrome.tabs.Tab & { title: string };
+
+export function faviconRenderer(
+  _instance: never,
+  td: HTMLTableCellElement,
+  _row: never,
+  _col: never,
+  _prop: never,
+  value: string
+): HTMLTableCellElement {
+  const escaped = Handsontable.helper.stringify(value);
+
+  if (escaped.indexOf("http") === 0) {
+    const img = document.createElement("img");
+    img.src = value;
+    img.height = 20;
+    img.width = 20;
+
+    Handsontable.dom.empty(td);
+    td.appendChild(img);
+  } else {
+    // render nothing
+  }
+
+  return td;
+}
+
+function App(): JSX.Element | string {
+  const [chromeTabs, setChromeTabs] = useState(null as Array<ChromeTab> | null);
   useEffect(() => {
-    chrome.tabs.query({}, tabs => setChromeTabs(tabs));
+    chrome.tabs.query({}, (tabs: Array<ChromeTab>) => setChromeTabs(tabs));
   }, []);
 
-  return (
-    <div>
-      {(() => {
-        const keys = new Set();
-        for (const tab of chromeTabs || []) {
-          for (const key of Object.keys(tab)) {
-            keys.add(key);
-          }
-        }
-        return JSON.stringify([...keys]);
-      })()}
-    </div>
+  return chromeTabs ? (
+    <HotTable
+      licenseKey="non-commercial-and-evaluation"
+      data={chromeTabs.map((tab: ChromeTab) => ({
+        faviconUrl: tab.favIconUrl,
+        title: tab.title
+      }))}
+      columns={[
+        { data: "faviconUrl", renderer: faviconRenderer, readOnly: true },
+        { data: "title", readOnly: true }
+      ]}
+      colHeaders={["", "Title"]}
+      colWidths={[50, 400]}
+    />
+  ) : (
+    "Loading..."
   );
 }
 
