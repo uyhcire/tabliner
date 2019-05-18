111557bf298ec6ec1d8adddbcf4651b103eb4ff9
uyhcire <ericyu3+uyhcire@gmail.com>
2019-05-17 18:20:01 -0700

    Keep track of the selected index instead of ID

--- src/main.tsx
+++ src/main.tsx
@@ -36,7 +36,7 @@ function useChromeTabs(): {
 
 function App(): JSX.Element {
   const { chromeTabs, setChromeTabs, setLocked } = useChromeTabs();
-  const [selectedTabId, setSelectedTabId] = useState(null as number | null);
+  const [selectedTabIndex, setSelectedTabIndex] = useState<number | null>(null);
 
   return (
     <div>
@@ -44,27 +44,46 @@ function App(): JSX.Element {
         <div
           tabIndex={0}
           onKeyDown={e => {
-            if (e.key === "Backspace" && selectedTabId) {
-              setLocked(true);
-              chrome.tabs.remove(selectedTabId, () => {
-                setChromeTabs(
-                  chromeTabs.filter(tab => tab.id !== selectedTabId)
-                );
-                setLocked(false);
-              });
+            switch (e.key) {
+              case "Backspace":
+                if (selectedTabIndex != null) {
+                  const selectedTabId = chromeTabs[selectedTabIndex].id;
+                  if (selectedTabId == null) {
+                    break;
+                  }
+                  setLocked(true);
+
+                  chrome.tabs.remove(selectedTabId, () => {
+                    setChromeTabs(
+                      chromeTabs.filter(tab => tab.id !== selectedTabId)
+                    );
+                    setSelectedTabIndex(
+                      Math.min(selectedTabIndex, chromeTabs.length - 1 - 1)
+                    );
+                    setLocked(false);
+                  });
+                }
+                break;
+              default:
+              // Do nothing
             }
           }}
         >
           <Tree
             contents={chromeTabs.map(
-              (tab): ITreeNode => ({
-                id: String(tab.id),
+              (tab, i): ITreeNode => ({
+                id: tab.id != null ? String(tab.id) : `undefined-tab-id-${i}`,
                 icon: <img src={tab.favIconUrl} height={20} width={20} />,
                 label: tab.title,
-                isSelected: tab.id === selectedTabId
+                isSelected: i === selectedTabIndex
               })
             )}
-            onNodeClick={node => setSelectedTabId(Number(node.id))}
+            onNodeClick={(_node, nodePath) => {
+              if (nodePath.length !== 1) {
+                throw new Error("Expected tab list to be flat");
+              }
+              setSelectedTabIndex(nodePath[0]);
+            }}
           />
         </div>
       ) : (
