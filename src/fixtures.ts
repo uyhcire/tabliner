7b8ecb699a05e21a6b318aabf13e8af1a5844d01
uyhcire <ericyu3+uyhcire@gmail.com>
2019-05-20 18:12:46 -0700

    Test moving a tab across multiple tabs

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -1,25 +1,15 @@
 import { ChromeTab } from "./ChromeTab";
 
-export const CHROME_TABS: Array<ChromeTab & { id: number }> = [
-  {
-    id: 1,
-    index: 0,
-    windowId: 1,
-    selected: true,
-    highlighted: true,
-    active: true,
-    pinned: false,
-    discarded: false,
-    autoDiscardable: false,
-    url: "https://google.com",
-    title: "Google",
-    favIconUrl: "https://example.com/favicon.ico",
-    status: "complete",
-    incognito: false
-  },
-  {
-    id: 2,
-    index: 1,
+export function makeChromeTabs(
+  tabInfos: Array<{ url: string; title: string }>
+): Array<ChromeTab & { id: number }> {
+  if (tabInfos.length === 0) {
+    throw new Error("At least one tab is required");
+  }
+
+  const tabs = tabInfos.map((tabInfo, i) => ({
+    id: i,
+    index: i,
     windowId: 1,
     selected: false,
     highlighted: false,
@@ -27,10 +17,21 @@ export const CHROME_TABS: Array<ChromeTab & { id: number }> = [
     pinned: false,
     discarded: false,
     autoDiscardable: false,
-    url: "https://yahoo.com",
-    title: "Yahoo",
+    url: tabInfo.url,
+    title: tabInfo.title,
     favIconUrl: "https://example.com/favicon.ico",
     status: "complete",
     incognito: false
-  }
-];
+  }));
+
+  tabs[0].selected = true;
+  tabs[0].highlighted = true;
+  tabs[0].active = true;
+
+  return tabs;
+}
+
+export const CHROME_TABS: Array<ChromeTab & { id: number }> = makeChromeTabs([
+  { url: "https://google.com", title: "Google" },
+  { url: "https://yahoo.com", title: "Yahoo" }
+]);
--- src/useChromeTabs.test.tsx
+++ src/useChromeTabs.test.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { act } from "react-dom/test-utils";
 
 import { useChromeTabs } from "./useChromeTabs";
-import { CHROME_TABS } from "./fixtures";
+import { CHROME_TABS, makeChromeTabs } from "./fixtures";
 import { ChromeTab } from "ChromeTab";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -35,26 +35,26 @@ interface MockEvent<ListenerType> {
   removeListener: (cb: ListenerType) => void;
 }
 
-declare let global: NodeJS.Global & {
-  chrome?: {
-    tabs: {
-      query: typeof chrome.tabs.query;
-      remove: typeof chrome.tabs.remove;
-      onMoved: MockEvent<TabMovedListener>;
-      onRemoved: MockEvent<TabRemovedListener>;
-    };
+interface MockChromeApi {
+  tabs: {
+    query: typeof chrome.tabs.query;
+    remove: typeof chrome.tabs.remove;
+    onMoved: MockEvent<TabMovedListener>;
+    onRemoved: MockEvent<TabRemovedListener>;
   };
+}
+
+declare let global: NodeJS.Global & {
+  chrome?: MockChromeApi;
 };
 
-let onMovedListeners: Array<TabMovedListener> = [];
+let onMovedListeners: Array<TabMovedListener>;
 let onRemovedListeners: Array<TabRemovedListener>;
-beforeEach(() => {
-  onMovedListeners = [];
-  onRemovedListeners = [];
-  global.chrome = {
+function getMockChromeTabsApi(tabs: Array<ChromeTab>): MockChromeApi {
+  return {
     tabs: {
       query: (_options: never, cb: (tabs: Array<ChromeTab>) => void) => {
-        cb(CHROME_TABS);
+        cb(tabs);
       },
       remove: jest.fn(),
       onMoved: {
@@ -79,6 +79,12 @@ beforeEach(() => {
       }
     }
   };
+}
+
+beforeEach(() => {
+  onMovedListeners = [];
+  onRemovedListeners = [];
+  global.chrome = getMockChromeTabsApi(CHROME_TABS);
 });
 
 it("returns null initially", () => {
@@ -132,6 +138,45 @@ it("reorders tabs in the list when they are moved", () => {
   expect(wrapper.find(MockChildComponent).props().chromeTabs).toEqual([
     { ...CHROME_TABS[1], index: 0 },
     { ...CHROME_TABS[0], index: 1 }
+  ]);
+});
+
+it("reorders tabs when a tab is moved across multiple tabs", () => {
+  // I dragged the 1st tab to be 6th and recorded the events Chrome fired
+  const tabs = makeChromeTabs([
+    { title: "1", url: "https://example.com/1" },
+    { title: "2", url: "https://example.com/2" },
+    { title: "3", url: "https://example.com/3" },
+    { title: "4", url: "https://example.com/4" },
+    { title: "5", url: "https://example.com/5" },
+    { title: "6", url: "https://example.com/6" }
+  ]);
+  global.chrome = getMockChromeTabsApi(tabs);
+  const wrapper = mount(<MockComponent />);
+  expect(onMovedListeners).toHaveLength(1);
+  act(() => {
+    for (const [fromIndex, toIndex] of [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5]
+    ]) {
+      onMovedListeners[0](tabs[0].id, {
+        fromIndex,
+        toIndex,
+        windowId: tabs[0].windowId
+      });
+    }
+  });
+  wrapper.update();
+  expect(wrapper.find(MockChildComponent).props().chromeTabs).toEqual([
+    { ...tabs[1], index: 0 },
+    { ...tabs[2], index: 1 },
+    { ...tabs[3], index: 2 },
+    { ...tabs[4], index: 3 },
+    { ...tabs[5], index: 4 },
+    { ...tabs[0], index: 5 }
   ]);
 });
 
