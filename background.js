f7cffba2a783e8f806051756e7b2ed85b1a62710
uyhcire <ericyu3+uyhcire@gmail.com>
2019-05-23 20:06:03 -0700

    Open the extension when Ctrl+Period is pressed

--- background.js
+++ background.js
@@ -1,3 +1,23 @@
 chrome.browserAction.onClicked.addListener(() => {
   chrome.tabs.create({ url: "index.html" });
 });
+
+let extensionTabId = null;
+chrome.commands.onCommand.addListener(function(command) {
+  if (command === "open") {
+    if (extensionTabId != null) {
+      chrome.tabs.update(extensionTabId, { active: true }, tab => {
+        chrome.windows.update(tab.windowId, { focused: true });
+      });
+    } else {
+      chrome.tabs.create({ url: "index.html" }, tab => {
+        extensionTabId = tab.id;
+      });
+    }
+  }
+});
+chrome.tabs.onRemoved.addListener(function(tabId) {
+  if (extensionTabId === tabId) {
+    extensionTabId = null;
+  }
+});
